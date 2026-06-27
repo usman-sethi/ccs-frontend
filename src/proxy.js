0610d9c5c0e5966193d27d361a6dd77c11545064
Musa-Stark <musa.fullstack08@gmail.com>
2026-06-27 18:44:43 +0500

    printing all cookie and req

--- src/proxy.js
+++ src/proxy.js
@@ -20,7 +20,8 @@ const verifyToken = async (token) => {
 };
 
 export async function proxy(req) {
-  console.log("cookies: ", req.cookies.getAll())
+  console.log("Cookie header:", req.headers.get("cookie"));
+console.log("All cookies:", req.cookies.getAll());
   const token = req.cookies.get("authToken")?.value;
   const { pathname } = req.nextUrl;
 
